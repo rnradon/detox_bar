625faa3c3fec84d2aee8512d2df1b3369a2409d1
Rishabh Narang <rnradon17@gmail.com>
2017-10-04 08:14:57 +0530

    machine and gym_user db changed

--- client/js/app1.js
+++ client/js/app1.js
@@ -14,10 +14,69 @@
                 templateUrl : "Views/login.html",
                 // showHeader : false,
             })
-            .when("/view_machines:userId?", {
-                templateUrl : "Views/view_machines.html",
+            .when("/admin:userId?", {
+                templateUrl : "Views/admin_landing_page.html",
+                // showHeader : true,
+            })
+            .when("/add_gym_user:userId?", {
+                templateUrl : "Views/register_user.html",
                 
-                controller: 'view_machines_controller'
+            })
+            .when("/del_gym_user:userId?", {
+                templateUrl : "Views/del_user.html",
+                
+            })
+            .when("/del_gym_user/user_found/id=:id/:access_token", {
+                templateUrl : "Views/del_user_redirect.html",
+                
+                controller: 'delete_user_controller'
+            })
+            .when("/edit_gym_user:userId?", {
+                templateUrl : "Views/edit_user.html",
+                
+            })
+            .when("/edit_gym_user/user_found/id=:id/:access_token", {
+                templateUrl : "Views/edit_user_redirect.html",
+                
+                controller: 'edit_user_controller'
+            })
+            .when("/view_gym_users:userId?", {
+                templateUrl : "Views/view_users.html",
+                
+                controller: 'view_users_controller'
+            })
+            .when("/search/id=:search_key/:access_token?", {
+                templateUrl : "Views/view_user_search.html",
+                
+                controller: 'search_user_controller'
+            })
+            .when("/health_analytics/id=:id/:access_token", {
+                templateUrl : "Views/health_analytics.html",
+                
+                controller: 'health_analytics_controller'
+            })
+            //gym user password reset
+            .when("/change_gym_user_password", {
+                templateUrl : "Views/change_gym_user_password.html"
+            })
+            //admin password reset
+            .when("/change_auth_password:userId?", {
+                templateUrl : "Views/change_auth_password.html",
+
+                controller: 'change_auth_password'
+            })
+            .when("/forgot_password", {
+                templateUrl : "Views/change_auth_password.html"
+            })
+            .when("/charts", {
+                templateUrl : "Views/analytics_chart_user_reg_num.html",
+                
+                // controller: 'edit_user_controller'
+            })
+            .when("/analytics_gym_user/user_found/gymUserId=:id/:access_token", {
+                templateUrl : "Views/analytics_chart_user.html",
+                
+                controller: 'analytics_user_controller'
             });
 
 
@@ -70,11 +129,11 @@
     }]);
 
 
-     app.controller('view_machines_controller', ['$routeParams', '$scope', '$http', function($routeParams, $scope, $http ) {
+     app.controller('view_users_controller', ['$routeParams', '$scope', '$http', function($routeParams, $scope, $http ) {
         
     
         var access_token = $routeParams.access_token
-        $http.get('/api/machines?access_token=' + access_token)
+        $http.get('/api/gym_users?access_token=' + access_token)
         .then(function(response) {
         $scope.users_json_data = response.data;
 
@@ -84,7 +143,7 @@
         // alert("CONTROLLER WORKS")
         });
 
-        $http.get('/api/machines/count?access_token=' + access_token)
+        $http.get('/api/gym_users/count?access_token=' + access_token)
         .then(function(response) {
         $scope.count = response.data;
 
